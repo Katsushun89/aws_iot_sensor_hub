c7ed03d7ede832cb474a078333ced4859ee715b2
Katsushun89 <Katsushun89@gmail.com>
2022-03-13 15:15:40 +0900

    Set port config.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 class Constants:
     COMPORT = '/dev/ttyUSB0'
     ENDPOINT = 'aaaaaaaa.iot.ap-northeast-1.amazonaws.com'
-    PORT = 9999
+    PORT = 8883
     ROOTCA = 'cert/AmazonRootCA1.pem'
     CERT = 'cert/certificate.pem.crt'
     PRIVATEKEY = 'cert/private.pem.key'
